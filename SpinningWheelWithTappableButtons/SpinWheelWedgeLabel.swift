25ba2973673302076fcc8eab35a65521b0fc0339
Admin <admin@Mac-Admin.local>
2017-10-06 17:36:21 +0300

    Make the buttons interact to a touchupinside event.

---
+++ SpinningWheelWithTappableButtons/SpinWheelWedgeLabel.swift
@@ -0,0 +1,45 @@
+//
+//  SpinWheelWedgeLabel.swift
+//  SpinWheelPractice
+//
+//  Created by Josh Henry on 5/18/17.
+//  Copyright © 2017 Big Smash Software. All rights reserved.
+//
+
+import UIKit
+
+class SpinWheelWedgeButton: TornadoButton {
+    public func configureWedgeButton(index: UInt, width: CGFloat, position: CGPoint, radiansPerWedge: Radians) {
+        self.frame = CGRect(x: 0, y: 0, width: width, height: 30)
+        self.layer.anchorPoint = CGPoint(x: 2, y: 0.5)
+        self.layer.position = position
+        self.transform = CGAffineTransform(rotationAngle: radiansPerWedge * CGFloat(index) + CGFloat.pi + (radiansPerWedge / 2))
+        self.backgroundColor = .green
+        self.addTarget(self, action: #selector(pressed(_:)), for: .touchUpInside)
+    }
+    @IBAction func pressed(_ sender: TornadoButton){
+        print("If you see this you won")
+    }
+}
+
+open class SpinWheelWedgeLabel: UILabel {
+    
+    private func setDefaultValues() {
+        self.textColor = UIColor.white
+        self.shadowColor = UIColor.black
+        self.adjustsFontSizeToFitWidth = true
+        self.textAlignment = .center
+    }
+    
+    public func configureWedgeLabel(index: UInt, width: CGFloat, position: CGPoint, radiansPerWedge: Radians) {
+        self.frame = CGRect(x: 0, y: 0, width: width, height: 30)
+        self.layer.anchorPoint = CGPoint(x: 1.1, y: 0.5)
+        self.layer.position = position
+        self.transform = CGAffineTransform(rotationAngle: radiansPerWedge * CGFloat(index) + CGFloat.pi + (radiansPerWedge / 2))
+    
+        setDefaultValues()
+    }
+    
+
+    
+}
--- SpinningWheelWithTappableButtons/ViewController.swift
+++ SpinningWheelWithTappableButtons/ViewController.swift
@@ -1,25 +1,47 @@
-//
-//  ViewController.swift
-//  SpinningWheelWithTappableButtons
-//
-//  Created by Admin on 06/10/2017.
-//  Copyright © 2017 Admin. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class TornadoButton: UIButton{
+    override func hitTest(_ point: CGPoint, with event: UIEvent?) -> UIView? {
+        
+        let pres = self.layer.presentation()!
+        let suppt = self.convert(point, to: self.superview!)
+        let prespt = self.superview!.layer.convert(suppt, to: pres)
+        if (pres.hitTest(suppt)) != nil{
+            return self
+        }
+        return super.hitTest(prespt, with: event)
+    }
+    
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        let pres = self.layer.presentation()!
+        let suppt = self.convert(point, to: self.superview!)
+        return (pres.hitTest(suppt)) != nil
+    }
+}
 
+extension ViewController: SpinWheelControlDelegate, SpinWheelControlDataSource{
+    func numberOfWedgesInSpinWheel(spinWheel: SpinWheelControl) -> UInt {
+        return 10
+    }
+    
+    func wedgeForSliceAtIndex(index: UInt) -> SpinWheelWedge {
+        let wedge = SpinWheelWedge()
+        return wedge
+    }
+}
+
+class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        spinWheel.dataSource = self
+        spinWheel.delegate = self
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        spinWheel.reloadData()
     }
-
-
+    @IBOutlet weak var spinWheel: SpinWheelControl!
 }
 
